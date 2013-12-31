515a1196a7f5447753e6c2b29704b45b995323b4
Nickolay <23Jeg@mail.ru>
2013-12-31 16:42:34 +0300

    Update Beepiano.cs

--- Beepiano.cs
+++ Beepiano.cs
@@ -1,31 +1,35 @@
+
+      
+ 
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication2
+namespace Beepiano
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Welcome to SqueZ`s BeePiano");
-            Console.WriteLine("                           ");
-            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Green;                 //Немного
+            Console.WriteLine("Welcome to SqueZ`s Beepiano");             //    Интересного
+            Console.WriteLine("                           ");             //         И красивого
+            Console.ForegroundColor = ConsoleColor.Yellow;                //             Оформления
             while (true)
             {
             
-                 ConsoleKeyInfo cki;
-                 int CHC = 180;
+                 ConsoleKeyInfo cki; //Клавиша
+                 int CHC = 180;        //Начальная длительность ноты
 
 
 
             Console.TreatControlCAsInput = true;
             cki = Console.ReadKey();
-            if ((cki.Modifiers & ConsoleModifiers.Shift) != 0) CHC = 100; 
-            if (cki.Key == ConsoleKey.D1) Console.Beep(100, CHC);
+            if ((cki.Modifiers & ConsoleModifiers.Shift) != 0) CHC = 100;  //При нажатии на Shift длительность ноты уменьшается 
+            //Основные ноты
+            if (cki.Key == ConsoleKey.D1) Console.Beep(100, CHC);  
             if (cki.Key == ConsoleKey.D2) Console.Beep(200, CHC);
             if (cki.Key == ConsoleKey.D3) Console.Beep(300, CHC);
             if (cki.Key == ConsoleKey.D4) Console.Beep(400, CHC);
@@ -35,6 +39,8 @@ namespace ConsoleApplication2
             if (cki.Key == ConsoleKey.D8) Console.Beep(800, CHC);
             if (cki.Key == ConsoleKey.D9) Console.Beep(900, CHC);
             if (cki.Key == ConsoleKey.D0) Console.Beep(1000, CHC);
+            
+            //#(диез) ноты
             if (cki.Key == ConsoleKey.Q) Console.Beep(150, CHC);
             if (cki.Key == ConsoleKey.W) Console.Beep(250, CHC);
             if (cki.Key == ConsoleKey.E) Console.Beep(350, CHC);
@@ -51,6 +57,3 @@ namespace ConsoleApplication2
     }
     }
 }
-
-      
- 
